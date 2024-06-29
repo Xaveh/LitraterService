99a06bbb0be275b13ac9e6d62440221f6b5f7c15
Xaveh <cbg9501@gmail.com>
2024-06-29 19:36:58 +0200

    architecture tests

---
+++ test/Litrater.ArchitectureTests/ArchitectureTests.cs
@@ -0,0 +1,135 @@
+using FluentAssertions;
+using NetArchTest.Rules;
+
+namespace Litrater.ArchitectureTests;
+
+public class ArchitectureTests
+{
+    private const string ApplicationNamespace = "Litrater.Application";
+    private const string DomainNamespace = "Litrater.Domain";
+    private const string InfrastructureNamespace = "Litrater.Infrastructure";
+    private const string PresentationNamespace = "Litrater.Presentation";
+
+    [Fact]
+    public void Domain_ShouldNotHaveDependency_OnOtherProjects()
+    {
+        // Arrange
+        var domainAssembly = typeof(Domain.Class1).Assembly;
+
+        var otherProjects = new[]
+        {
+            ApplicationNamespace,
+            InfrastructureNamespace,
+            PresentationNamespace
+        };
+
+        // Act
+        var result = Types.InAssembly(domainAssembly)
+                          .ShouldNot()
+                          .HaveDependencyOnAny(otherProjects)
+                          .GetResult();
+
+        // Assert
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Application_ShouldNotHaveDependency_OnOtherProjects()
+    {
+        // Arrange
+        var applicationAssembly = typeof(Application.Class1).Assembly;
+
+        var otherProjects = new[]
+        {
+            InfrastructureNamespace,
+            PresentationNamespace
+        };
+
+        // Act
+        var result = Types.InAssembly(applicationAssembly)
+                          .ShouldNot()
+                          .HaveDependencyOnAny(otherProjects)
+                          .GetResult();
+
+        // Assert
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Infrastructure_ShouldNotHaveDependency_OnOtherProjects()
+    {
+        // Arrange
+        var infrastructureAssembly = typeof(Infrastructure.Class1).Assembly;
+
+        var otherProjects = new[]
+        {
+            PresentationNamespace
+        };
+
+        // Act
+        var result = Types.InAssembly(infrastructureAssembly)
+                          .ShouldNot()
+                          .HaveDependencyOnAny(otherProjects)
+                          .GetResult();
+
+        // Assert
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Presentation_ShouldNotHaveDependency_OnOtherProjects()
+    {
+        // Arrange
+        var presentationAssembly = typeof(Presentation.Class1).Assembly;
+
+        var otherProjects = new[]
+        {
+            InfrastructureNamespace
+        };
+
+        // Act
+        var result = Types.InAssembly(presentationAssembly)
+                          .ShouldNot()
+                          .HaveDependencyOnAny(otherProjects)
+                          .GetResult();
+
+        // Assert
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Handlers_ShouldHaveDependency_OnDomain()
+    {
+        // Arrange
+        var applicationAssembly = typeof(Application.Class1).Assembly;
+
+        // Act
+        var result = Types.InAssembly(applicationAssembly)
+                          .That()
+                          .HaveNameEndingWith("Handler")
+                          .Should()
+                          .HaveDependencyOn(DomainNamespace)
+                          .GetResult();
+
+        // Assert
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Controllers_ShouldHaveDependency_OnMediatR()
+    {
+        // Arrange
+        var presentationAssembly = typeof(Presentation.Class1).Assembly;
+
+        // Act
+        var result = Types.InAssembly(presentationAssembly)
+                          .That()
+                          .HaveNameEndingWith("Controller")
+                          .Should()
+                          .HaveDependencyOn("MediatR")
+                          .GetResult();
+
+        // Assert
+        result.IsSuccessful.Should().BeTrue();
+    }
+}
