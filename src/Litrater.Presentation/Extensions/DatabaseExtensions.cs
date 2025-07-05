ee1ddd260de28d469c23867570816091b920b982
Xaveh <cbg9501@gmail.com>
2025-07-05 20:31:10 +0200

    Split database setup into migration and seeding operations.
    - Added `MigrateDatabaseAsync` extension for applying migrations.
    - Renamed and refactored database seeding to `SeedDatabaseAsync`.
    - Updated `Program` to execute migrations and seeding sequentially during startup.

--- src/Litrater.Presentation/Extensions/DatabaseExtensions.cs
+++ src/Litrater.Presentation/Extensions/DatabaseExtensions.cs
@@ -1,10 +1,19 @@
 using Litrater.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Litrater.Presentation.Extensions;
 
 internal static class DatabaseExtensions
 {
-    public static async Task EnsureDatabaseSeededAsync(this IServiceProvider serviceProvider)
+    public static async Task MigrateDatabaseAsync(this IServiceProvider serviceProvider)
+    {
+        using var scope = serviceProvider.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<LitraterDbContext>();
+
+        await context.Database.MigrateAsync();
+    }
+
+    public static async Task SeedDatabaseAsync(this IServiceProvider serviceProvider)
     {
         using var scope = serviceProvider.CreateScope();
         var context = scope.ServiceProvider.GetRequiredService<LitraterDbContext>();
