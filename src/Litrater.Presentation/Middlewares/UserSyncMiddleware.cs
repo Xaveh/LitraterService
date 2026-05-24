8f4db76c40cee6ad3120615cac9c325a7a14e9f6
Xaveh <cbg9501@gmail.com>
2026-05-24 16:01:10 +0200

    Refactor `UserSyncMiddleware` to simplify dependency management by removing service provider usage

--- src/Litrater.Presentation/Middlewares/UserSyncMiddleware.cs
+++ src/Litrater.Presentation/Middlewares/UserSyncMiddleware.cs
@@ -5,12 +5,9 @@ using Litrater.Domain.Users;
 
 namespace Litrater.Presentation.Middlewares;
 
-internal sealed class UserSyncMiddleware(
-    RequestDelegate next,
-    IServiceProvider serviceProvider,
-    ILogger<UserSyncMiddleware> logger)
+internal sealed class UserSyncMiddleware(RequestDelegate next, ILogger<UserSyncMiddleware> logger)
 {
-    public async Task InvokeAsync(HttpContext context)
+    public async Task InvokeAsync(HttpContext context, IUserRepository userRepository, IUnitOfWork unitOfWork)
     {
         if (context.User.Identity?.IsAuthenticated == true)
         {
@@ -22,10 +19,6 @@ internal sealed class UserSyncMiddleware(
                 await next(context);
                 return;
             }
-
-            using var scope = serviceProvider.CreateScope();
-            var userRepository = scope.ServiceProvider.GetRequiredService<IUserRepository>();
-            var unitOfWork = scope.ServiceProvider.GetRequiredService<IUnitOfWork>();
 
             var existingUser = await userRepository.GetByKeycloakUserIdAsync(keycloakUserId);
             if (existingUser is not null)
